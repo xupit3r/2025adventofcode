eaba6b1ec82b7cc1d4156214f50f9bd6ff233d1f
Joe D'Alessandro <joe@thejoeshow.net>
2026-01-07 17:03:09 -0500

    day 2 part 2.

--- day2.mjs
+++ day2.mjs
@@ -3,24 +3,48 @@
 import { readFile } from 'node:fs/promises';
 
 const input = await readFile('./inputs/day2.txt', 'utf-8');
-const invalidSum = input.split(',').map(range => {
+
+const generateRangeValues = (range) => {
   const [ lower, upper ] = range.split('-');
-  const invalids = [];
+  const values = [];
 
   for (var i = Number(lower); i <= Number(upper); i++) {
-    const iStr = "" + i;
-    const halfway = Math.ceil(iStr.length / 2);
-    const p1 = iStr.substring(0, halfway);
-    const p2 = iStr.substring(halfway);
-
-    if (p1 == p2) {
-      invalids.push(i);
-    }
+    values.push(i);
   }
 
-  return invalids;
-}).reduce((sum, invalids) => {
-  return sum + invalids.reduce((s, v) => s + v, 0);
-}, 0);
+  return values;
+}
 
-console.log(invalidSum);
+const sumInvalids = (sum, invalids) => {
+  return sum + invalids.reduce((s, v) => s + v, 0);
+};
+
+const part1 = (data) => data.split(',').map(range => {
+  return generateRangeValues(range).map(value => {
+    const valueStr = "" + value;
+    const halfway = Math.ceil(valueStr.length / 2);
+    const p1 = valueStr.substring(0, halfway);
+    const p2 = valueStr.substring(halfway);
+
+    if (p1 == p2) {
+      return value;
+    }
+
+    return -1;
+  }).filter(v => v > -1);
+}).reduce(sumInvalids, 0);
+
+const part2 = (data) => data.split(',').map(range => {
+  return generateRangeValues(range).map(value => {
+    const valueStr = "" + value;
+    const twice = valueStr + valueStr;
+
+    if (twice.indexOf(valueStr, 1) < valueStr.length) {
+      return value;
+    }
+
+    return -1;
+  }).filter(v => v > -1);
+}).reduce(sumInvalids, 0);
+
+console.log(part2(input));
