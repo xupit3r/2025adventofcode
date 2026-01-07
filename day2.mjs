78c85d52bce1f0ea9bb70278a134c078ee51de74
Joe D'Alessandro <joe@thejoeshow.net>
2026-01-07 16:37:03 -0500

    day 2 part 1.

---
+++ day2.mjs
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+import { readFile } from 'node:fs/promises';
+
+const input = await readFile('./inputs/day2.txt', 'utf-8');
+const data = input.split(',').map(range => {
+  const [ lower, upper ] = range.split('-');
+  const invalids = [];
+
+  for (var i = Number(lower); i <= Number(upper); i++) {
+    const iStr = "" + i;
+    const halfway = Math.ceil(iStr.length / 2);
+    const p1 = iStr.substring(0, halfway);
+    const p2 = iStr.substring(halfway);
+
+    if (p1 == p2) {
+      invalids.push(i);
+    }
+  }
+
+  return invalids;
+}).reduce((sum, invalids) => {
+  return sum + invalids.reduce((s, v) => s + v, 0);
+}, 0);
+
+console.log(data);
