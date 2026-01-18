c855766d9fb28d731116a7af852808c8580a03cb
Joe D'Alessandro <joe@thejoeshow.net>
2026-01-18 13:59:42 -0500

    day 5, part 2

--- day5.mjs
+++ day5.mjs
@@ -3,19 +3,8 @@
 import { readFile } from 'node:fs/promises';
 
 const input = await readFile('./inputs/day5.txt', 'utf-8');
-// const input = `3-5
-// 10-14
-// 16-20
-// 12-18
 
-// 1
-// 5
-// 8
-// 11
-// 17
-// 32`;
-
-const parse = (input) => {
+const buildLookup = (input) => {
   return input.split('\n').reduce((result, row) => {
     if (row.indexOf('-') > -1) {
       const [lower, upper] = row.split('-').map(Number);
@@ -32,10 +21,10 @@ const parse = (input) => {
 }
 
 const part1 = (input) => {
-  const { fresh, ids } = parse(input);
+  const { fresh, ids } = buildLookup(input);
   return ids.filter(id => {
     let found = false;
-    
+
     for (let i = 0; i < fresh.length && !found; i++) {
       found = fresh[i](id);
     }
@@ -44,4 +33,30 @@ const part1 = (input) => {
   }).length;
 }
 
-console.log(part1(input));
+const part2 = (input) => {
+  const ranges = input.split('\n')
+                      .filter(row => row.indexOf('-') > -1)
+                      .map(row => row.split('-').map(Number))
+                      .sort((a, b) => a[0] - b[0]);
+  const result = [];
+  let lower = sorted[0][0];
+  let upper = sorted[0][1];
+
+  for (let i = 1; i < ranges.length; i++) {
+    const current = ranges[i];
+
+    if (current[0] > upper) {
+      result.push([ lower, upper ]);
+      lower = current[0];
+      upper = current[1];
+    } else if (current[1] > upper) {
+      upper = current[1];
+    }
+  }
+
+  result.push([ lower, upper ]);
+
+  return result.reduce((sum, [ lower, upper ]) => sum + (upper - lower) + 1, 0);
+}
+
+console.log(part2(input));
